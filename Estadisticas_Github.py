5c6344792c917eb666b95538e8ce282de14a8666
unknown <sebasguesa@hotmail.com>
2026-02-10 23:39:32 -0500

    Se actualiza el readme del github sin token personal

--- Estadisticas_Github.py
+++ Estadisticas_Github.py
@@ -9,8 +9,8 @@ from collections import defaultdict
 from datetime import datetime
 
 # Se crean las variables de usuario y token
-TOKEN = "TOKEN_"
-USER = "USUARIO_"
+TOKEN = "_TOKEN_"
+USER = "_USUARIO_"
 
 # Se crea la variable con los json necesarios
 HEADERS = {
